c1c1a0de4a384fc39e3a23a0889b137929d62f69
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-09-30 02:19:35 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
-
-
+class Book 
+  attr_accessor :page_count, :genre 
+  def initialize(book_title)
+    @title = book_title 
+  end
+  def title 
+    @title 
+  end
+  def author 
+    @author 
+  end
+  def author=(author_name)
+    @author = author_name
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
